52400fa812ca90e98ef723c7fe78334f14b3f6f3
00edgar <shyzedgar@gmail.com>
2026-08-13 16:39:39 -0600

    crear tabla pedidos

--- db_distribuidora/ddl/schema.sql
+++ db_distribuidora/ddl/schema.sql
@@ -39,3 +39,19 @@ CREATE TABLE IF NOT EXISTS sedes (
     capacidad_almacenamiento INT NOT NULL CHECK (capacidad_almacenamiento > 0),
     encargado VARCHAR(150) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+
+-- ============================================================
+-- TABLA 4: pedidos
+-- ============================================================
+CREATE TABLE IF NOT EXISTS pedidos (
+    id_pedido INT AUTO_INCREMENT PRIMARY KEY,
+    fecha_pedido DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    id_cliente INT NOT NULL,
+    id_sede INT NOT NULL,
+    total_sin_iva DECIMAL(12,2) NOT NULL DEFAULT 0.00 CHECK (total_sin_iva >= 0),
+    total_con_iva DECIMAL(12,2) NOT NULL DEFAULT 0.00 CHECK (total_con_iva >= 0),
+    CONSTRAINT fk_pedidos_cliente FOREIGN KEY (id_cliente) REFERENCES clientes(id_cliente) ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT fk_pedidos_sede FOREIGN KEY (id_sede) REFERENCES sedes(id_sede) ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
