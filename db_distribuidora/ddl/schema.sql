297ff2e2ec7b9a41f6a87a2761761ceba7970051
00edgar <shyzedgar@gmail.com>
2026-08-13 16:34:29 -0600

    crear base de datos

---
+++ db_distribuidora/ddl/schema.sql
@@ -0,0 +1,5 @@
+-- ============================================================
+-- BASE DE DATOS: db_distribuidora
+-- ============================================================
+CREATE DATABASE IF NOT EXISTS db_distribuidora;
+USE db_distribuidora;
